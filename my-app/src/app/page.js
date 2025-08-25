497ef9e44ea0eea078c0ad1767c0ed1a67f65bf0
att att <77044381+attacker0258@users.noreply.github.com>
2025-08-25 09:18:32 +0000

    TEST

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-100">
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="text-xl font-bold">TEST Rate limit</h1>
+        <button onclick={}  className="w-max border p-2 rounded cursor-pointer hover:bg-gray-100">TEST</button>
+      </div>
+    </div>
+  );
+}
---
+++ my-app/src/app/rate/route.js
@@ -0,0 +1,61 @@
+import { RateLimiterMemory } from "rate-limiter-flexible";
+import { cookies } from "next/headers";
+
+// Per-IP limiter (stricter, to prevent mass abuse)
+const ipLimiter = new RateLimiterMemory({
+  points: 6,   // 6 requests
+  duration: 1000, // per day per IP
+});
+
+// Per-anonId limiter (fairness per device/browser)
+const guestLimiter = new RateLimiterMemory({
+  points: 2,   // 6 requests
+  duration: 1000, // per day per guestId
+});
+
+export async function GET(req) {
+  const cookieStore = cookies();
+
+  // --- Get IP ---
+  const ip =
+    req.headers.get("x-forwarded-for")?.split(",")[0]?.trim() ||
+    req.headers.get("x-real-ip") ||
+    "127.0.0.1";
+
+  // --- Get/Create guestId ---
+  let guestId = cookieStore.get("guest_id")?.value;
+  let setNewCookie = false;
+
+  if (!guestId) {
+    guestId = crypto.randomUUID();
+    setNewCookie = true;
+  }
+
+  try {
+    // Apply both limiters
+    await guestLimiter.consume(guestId, 1);  // 1 points per request
+    await ipLimiter.consume(ip, 1);        // 1 point per request
+
+    // --- Success ---
+    const res = Response.json({ message: "hi" });
+
+    // If new guestId, set cookie
+    if (setNewCookie) {
+      res.headers.append(
+        "Set-Cookie",
+        `guest_id=${guestId}; Path=/; HttpOnly; SameSite=Strict; Max-Age=31536000` // 1 year
+      );
+    }
+
+    return res;
+
+  } catch (rej) {
+    // --- Blocked ---
+    return new Response("Too Many Requests", {
+      status: 429,
+      headers: {
+        "Retry-After": Math.ceil(rej.msBeforeNext / 1000), // hint for client
+      },
+    });
+  }
+}
